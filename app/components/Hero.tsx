c43a71ed79ab3b42ad537e51807bea07e0141af0
JieLuis <lionliao1658@gmail.com>
2025-04-02 22:05:26 +0800

    Update: optimize selfie and hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,15 +4,11 @@ import selfie from "@/public/images/me2.png" // Update the image import
 import Image from "next/image"
 import { TypeAnimation } from "react-type-animation"
 import { motion } from "framer-motion"
-import { ServerComponent, ClientComponent } from "shiki-components"
-import { useStore } from "zustand"
 import { ThemeContext } from "../context/DarkModeContext"
-import CodeBlocker from "./CodeBlocker"
-import classNames from "classnames"
+import { CodeBlocker } from "shiki-test"
 
 const Hero = () => {
   const themeContext = useContext(ThemeContext)
-  const [isHovered, setIsHovered] = useState(false)
   let timeoutId: NodeJS.Timeout | null = null
 
   if (!themeContext) {
@@ -40,52 +36,46 @@ const Hero = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-7 col-start-1 flex items-center justify-start relative sm:left-40"
+          className="sm:col-start-2 sm:col-end-7 flex items-start relative"
         >
-          <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-4 text-yellow-500">
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              {`Heya I'm `}
-            </span>
-            <TypeAnimation
-              sequence={[
-                // Same substring at the start will only be typed out once, initially
-                "Jie",
-                1500, // wait 1s before replacing "Mice" with "Hamsters"
-                "a web developer",
-                1500,
-                "廖永杰",
-                1500,
-              ]}
-              wrapper="div"
-              speed={50}
-              repeat={Infinity}
-            />
-          </h1>
+          <div>
+            <h1 className="text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-4 text-yellow-500">
+              <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+                {`Heya I'm `}
+              </span>
+              <TypeAnimation
+                sequence={[
+                  "Jie",
+                  1500,
+                  "a web developer",
+                  1500,
+                  "廖永杰",
+                  1500,
+                ]}
+                wrapper="div"
+                speed={50}
+                repeat={Infinity}
+                className="mt-3" /** To offset image top space */
+              />
+            </h1>
+            <div className="w-[370px] md:w-[440px] lg:w-[500px]">
+              <CodeBlocker code={code} colorMode={colorMode} />
+            </div>
+          </div>
         </motion.div>
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-5 place-self-center mt-4 lg:mt-0"
+          className="sm:col-start-8"
         >
-          <div
-            style={{
-              height: "500px",
-              width: "500px",
-            }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
+          <div className="w-[500px] relative bottom-[60px]">
             <Image
               src={selfie}
               alt="Jie is standing firm"
-              width={300}
+              width={350}
               height={300}
-              className={classNames({
-                hidden: isHovered,
-              })}
             />
-            {isHovered && <CodeBlocker code={code} colorMode={colorMode} />}
           </div>
         </motion.div>
       </div>
