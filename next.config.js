9d5d88b12bae95c606ffaf855231df12ea16267d
JieLuis <lionliao1658@gmail.com>
2025-04-13 12:14:14 +0800

    Fix: defected pigment css config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-// Ensure you use .mjs extension if using ESM
-import { withPigment } from "@pigment-css/nextjs-plugin"
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-export default withPigment(nextConfig)
+module.exports = nextConfig
