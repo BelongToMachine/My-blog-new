e52921627dbd4ee5d893ac7a1cb70bb903c05908
J <lionliao1658@gmail.com>
2026-04-21 23:14:54 +0800

    refactor: redesign navigation components with mobile-responsive layout and custom pixel-art icons

--- app/components/navbar/StyleToggle.tsx
+++ app/components/navbar/StyleToggle.tsx
@@ -6,6 +6,36 @@ import { PiLayout } from "react-icons/pi"
 import { useStyleModeStore } from "@/app/service/Store"
 import { ActionIconButton } from "../system/ActionIconButton"
 
+const PixelSquaresFour = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    shapeRendering="crispEdges"
+    className="h-5 w-5"
+    fill="currentColor"
+  >
+    <rect x="2" y="2" width="8" height="8" />
+    <rect x="14" y="2" width="8" height="8" />
+    <rect x="2" y="14" width="8" height="8" />
+    <rect x="14" y="14" width="8" height="8" />
+  </svg>
+)
+
+const PixelLayout = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    shapeRendering="crispEdges"
+    className="h-5 w-5"
+    fill="currentColor"
+  >
+    <rect x="2" y="2" width="20" height="4" />
+    <rect x="2" y="8" width="8" height="14" />
+    <rect x="12" y="8" width="10" height="6" />
+    <rect x="12" y="16" width="10" height="6" />
+  </svg>
+)
+
 const StyleToggle = () => {
   const { styleMode, toggleStyleMode } = useStyleModeStore()
 
@@ -22,7 +52,9 @@ const StyleToggle = () => {
       type="button"
       tone="borderless"
     >
-      {styleMode === "pixel" ? <PiSquaresFour size={18} /> : <PiLayout size={18} />}
+      {styleMode === "pixel"
+        ? <PixelSquaresFour />
+        : <PixelLayout />}
     </ActionIconButton>
   )
 }
